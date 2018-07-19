440d0fab2da9914c48f95dd2efe5fbcdd54c478f
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-19 17:26:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,11 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|calls| calls.capitalize + "!"}
+  planeteer_calls.collect {|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls(calls)
-  
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
